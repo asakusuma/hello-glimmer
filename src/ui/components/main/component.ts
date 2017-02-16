69f2a3b001873c26171a3dbd189d36a13a4cecdf
asakusuma <asa.kusuma@gmail.com>
2017-02-16 07:39:22 -0800

    Repro main component bug

---
+++ src/ui/components/main/component.ts
@@ -0,0 +1,9 @@
+import Component from '@glimmer/component';
+
+export default class Main extends Component {
+  constructor(args) {
+    debugger;
+    console.log('Main constructor', args);
+    super(args);
+  }
+};
