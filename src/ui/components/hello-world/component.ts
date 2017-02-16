2e1e8147b457d04656c6d5e995714d30f5139dd2
asakusuma <asa.kusuma@gmail.com>
2017-02-16 11:22:01 -0800

    Repro each issue

--- src/ui/components/hello-world/component.ts
+++ src/ui/components/hello-world/component.ts
@@ -1,8 +1,10 @@
 import Component from '@glimmer/component';
 
 export default class HelloWorld extends Component {
+  list: Array<string>;
   constructor(args) {
     console.log('HelloWorld constructor', args);
     super(args);
+    this.list = ['tom', 'robbie'];
   }
 };
